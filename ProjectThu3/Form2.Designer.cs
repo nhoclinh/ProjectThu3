2497bb3d746a1cf4c01c55342e4a2db1ad2ebd42
Bui Duc Duy <11235561@st.neu.edu.vn>
2025-09-09 16:54:55 +0700

    bờ lét thêm ae ơi

--- ProjectThu3/Form2.Designer.cs
+++ ProjectThu3/Form2.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -40,11 +41,21 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "my công việc";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(593, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 114);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "bless";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form2";
             this.Text = "Form2";
@@ -56,5 +67,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
